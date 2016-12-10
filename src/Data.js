d1a1f19faf1b810716a99340cff2554005e8ff9e
Ostap Maliuvanchuk <ostap@salemove.com>
2016-12-10 16:52:28 +0200

    Add ruby

--- src/Data.js
+++ src/Data.js
@@ -1,11 +1,12 @@
 import javaLogo from './images/java.jpg';
 import javascriptLogo from './images/javascript.svg';
+import rubyLogo from './images/ruby.png';
 
 export const languages = [
   {
     name: 'Java',
-    paradigms: ['object-oriented', 'functional', 'static'],
-    features: ['generics'],
+    paradigms: ['object-oriented', 'functional', 'static', 'compiled', 'reflective'],
+    features: ['generics', 'checked exceptions', 'garbage collection', 'case sensitive', 'lambdas'],
     description: `
       Java is a general-purpose computer programming language that is concurrent,
       class-based, object-oriented, and specifically designed to have as few implementation dependencies as possible.
@@ -14,8 +15,8 @@ export const languages = [
   },
   {
     name: 'JavaScript',
-    paradigms: ['object-oriented', 'functional', 'dynamic'],
-    features: ['generics'],
+    paradigms: ['object-oriented', 'functional', 'procedural', 'dynamic', 'prototype-based', 'interpreted', 'reflective'],
+    features: ['frontend', 'imperative', 'interactive mode', 'case sensitive', 'lambdas'],
     description: `
       JavaScript is a high-level, dynamic, untyped, and interpreted programming language.
       It has been standardized in the ECMAScript language specification.
@@ -23,5 +24,17 @@ export const languages = [
       the majority of websites employ it, and all modern Web browsers support it without the need for plug-ins.
     `,
     logo: javascriptLogo
+  },
+  {
+    name: 'Ruby',
+    paradigms: ['object-oriented', 'functional', 'imperative', 'dynamic', 'interpreted'],
+    features: [
+      'inherirance', 'mixins', 'metaclasses', 'duck typing', 'garbage collection', 'native threads',
+      'anonymous functions', 'closures', 'backend', 'interactive mode', 'case sensitive', 'lambdas'
+    ],
+    description: `Ruby is a dynamic, reflective, object-oriented, general-purpose programming language.
+      It was designed and developed in the mid-1990s by Yukihiro "Matz" Matsumoto in Japan.
+    `,
+    logo: rubyLogo
   }
 ];
